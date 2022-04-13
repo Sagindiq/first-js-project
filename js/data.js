715945e1b2373d5a8ddb00006baab6aefc204cf4
Sagindiq <sogindi77@gmail.com>
2022-04-13 22:47:06 +0500

    Sneakers render and sneaker add form enabled

--- js/data.js
+++ js/data.js
@@ -3,37 +3,92 @@
     id: 123,
     title: "Nike air jordan",
     img: "https://picsum.photos/300/200",
-    price: 350.000,
-    discountPrice: 350.000 * 0.75, // chegirmadagi summa.
+    price: 350000,
+    discountPrice: 350000 * 0.75, // chegirmadagi summa.
     model: "Nike",
     size: 43,
     addedDate: new Date("2021-11-12").toISOString(),
-    benefits: ["spring", "44", "Waterproof"]
+    benefits: ["best choice for autumn and spring", "44", "Waterproof"]
   },
   {
     id: 124,
     title: "Adidas NMD_R1",
     img: "https://picsum.photos/300/200",
-    discountPrice: 400.000 * 0.75, // chegirmadagi summa.
-    price: 400.000,
+    discountPrice: 400000 * 0.75, // chegirmadagi summa.
+    price: 400000,
     model: "Adidas",
     size: 42,
     addedDate: new Date("2021-10-12").toISOString(),
-    benefits: ["winter", "1tb"]
+    benefits: ["best choice for winter", "1tb"]
   }
 ]
 
 const manufacturers = [
   {
     id: 1,
-    name: "Xiaomi"
+    name: "Nike"
   },
   {
     id: 2,
-    name: "Apple"
+    name: "Adidas"
   },
   {
     id: 3,
-    name: "Samsung"
+    name: "Converse"
+  },
+  {
+    id: 4,
+    name: "Reebok"
+  },
+  {
+    id: 5,
+    name: "New Balance"
+  },
+  {
+    id: 6,
+    name: "Vans"
+  },
+  {
+    id: 7,
+    name: "Puma"
+  },
+  {
+    id: 8,
+    name: "Jordan Brand"
   }
 ];
+
+const sizes = [
+  {
+    id: 1,
+    size: 38
+  },
+  {
+    id: 2,
+    size: 39
+  },
+  {
+    id: 3,
+    size: 40
+  },
+  {
+    id: 4,
+    size: 41
+  },
+  {
+    id: 5,
+    size: 42
+  },
+  {
+    id: 6,
+    size: 43
+  },
+  {
+    id: 7,
+    size: 44
+  },
+  {
+    id: 8,
+    size: 45
+  }
+]
---
+++ js/main.js
@@ -0,0 +1,239 @@
+const sneakerList = document.querySelector(".sneakers__list");
+
+const addElement = function (tagName, className, content) {
+    const newElement = document.createElement(tagName);
+
+    if (className) {
+        newElement.className = className;
+    }
+
+    if (content) {
+        newElement.textContent = content;
+    }
+
+    return newElement
+}
+
+const addImg = function (className, src) {
+    const newImg = document.createElement("img");
+    newImg.src = src;
+
+    if (className) {
+        newImg.className = className;
+    }
+
+    return newImg
+}
+
+const appendChild = function (parent, child) {
+    parent.append(child);
+}
+
+const appendChildren = function (parent, children) {
+    children.forEach(function (children) {
+        parent.append(children);
+    });
+}
+
+const addZero = function (number) {
+    return number < 10 ? "0" + number : number
+}
+
+const showDate = function (dateString) {
+    const date = new Date(dateString);
+
+    return `${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.${date.getFullYear()} ${addZero(date.getHours())}:${addZero(date.getMinutes())}`;
+}
+
+const renderSneakers = function (sneakers) {
+    const {
+        id,
+        title,
+        img,
+        price,
+        discountPrice,
+        model,
+        size,
+        addedDate,
+        benefits
+    } = sneakers
+
+    const sneakerItem = addElement("li", "col-4 sneakers__item selector-for-some-widget");
+
+    const sneakerCard = addElement("div", "card");
+    const sneakerImg = addImg("card-img-top", img);
+    const sneakerCardBody = addElement("div", "card-body");
+
+    const sneakerTitle = addElement("h3", "card-title", title);
+
+    const sneakerDiscountPrice = addElement("p", "card-text fw-bold");
+    const sneakerDiscountPriceMark = addElement("mark", "", discountPrice);
+
+    const sneakerPrice = addElement("p", "card-text text-decoration-line-through", price);
+
+    const sneakerMaker = addElement("p", "badge bg-success", model);
+
+    const sneakerSize = addElement("p", "d-block w-50 badge bg-info", `Size: ${size}`);
+
+    const sneakerAddedDate = addElement("p", "card-text", showDate(addedDate));
+
+    const sneakerBenefitsList = addElement("ul", "d-flex flex-wrap list-unstyled");
+
+    benefits.forEach(function(benefitsItem){
+        const sneakerBeenfitsItem = addElement("li", "badge bg-primary me-1 mb-1", benefitsItem);
+
+        appendChild(sneakerBenefitsList, sneakerBeenfitsItem);
+    })
+
+    appendChild(sneakerItem, sneakerCard); // div.card in li.sneakers__item
+    appendChild(sneakerDiscountPrice, sneakerDiscountPriceMark);
+
+    appendChildren(sneakerCard, [sneakerImg, sneakerCardBody]); // sneakerImg and sneakerCardBody in sneakerCard
+    appendChildren(sneakerCardBody, [sneakerTitle, sneakerDiscountPrice, sneakerPrice, sneakerMaker, sneakerSize, sneakerAddedDate, sneakerBenefitsList]);
+
+    // appendChild(sneakerList, sneakerItem);
+
+    return sneakerItem;
+}
+
+let showSneakers = sneakers.slice();
+const sneakersCount = document.getElementById("sneakers-count");
+
+const sneakerRefresh = function () {
+    sneakersCount.textContent = `count: ${sneakers.length}`;
+    sneakerList.innerHTML = "";
+
+    sneakers.forEach(function (sneakerItems) {
+        const sneakerItem = renderSneakers(sneakerItems);
+        appendChild(sneakerList, sneakerItem);
+    });
+}
+
+const sneakerMakers = document.querySelector(".sneakers-manufacturer-select");
+const sneakerSizes = document.querySelector(".sneakers-size");
+
+manufacturers.forEach(function (makers) {
+    const {
+        id,
+        name
+    } = makers
+
+    const sneakerManufacturer = document.createElement("option");
+    sneakerManufacturer.textContent = name;
+    sneakerManufacturer.value = name;
+
+    appendChild(sneakerMakers, sneakerManufacturer);
+});
+
+sizes.forEach(function (sneakersSizes) {
+    const {
+        id,
+        size
+    } = sneakersSizes
+
+    const sneakerSize = document.createElement("option");
+    sneakerSize.textContent = size;
+    sneakerSize.value = size;
+
+    appendChild(sneakerSizes, sneakerSize);
+
+})
+
+sneakerRefresh()
+
+const sneakerBenefitsInput = document.querySelector("#benefit");
+const sneeakerBenefitsExampleList = document.querySelector("#benefitsExample");
+
+benefitsOptions = []
+
+sneakerBenefitsInput.addEventListener("input", function () {
+
+    const benefitsItemValue = sneakerBenefitsInput.value;
+
+    const splitBenefits = benefitsItemValue.trim().split(",");
+
+    if (splitBenefits.length == 2) {
+        benefitsOptions.push(splitBenefits[0]);
+        sneakerBenefitsInput.value = "";
+
+        sneeakerBenefitsExampleList.textContent = "";
+
+        benefitsOptions.forEach(function (sneakerBenefits) {
+            const sneakerBeenfitsItem = addElement("li", "btn btn-sm badge rounded-pill btn-danger mr-5 benefits__item", sneakerBenefits);
+
+            appendChild(sneeakerBenefitsExampleList, sneakerBeenfitsItem);
+        })
+    }
+});
+
+const addSneakerBtn = document.querySelector(".addsneaker");
+
+addSneakerBtn.addEventListener("click", function () {
+    benefitsOptions = [];
+    sneakerBenefitsInput.value = "";
+    sneeakerBenefitsExampleList.innerHTML = "";
+});
+
+const addSneakerForm = document.querySelector("#add-sneaker");
+const addModal = document.querySelector("#add-sneaker-modal");
+const addSneakerModal = new bootstrap.Modal(addModal);
+
+addSneakerForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+
+    const elements = evt.target.elements;
+
+    const title = elements.title;
+    const price = elements.sneaker__price;
+    const makers = elements.sneaker__manufacturer;
+    const sizes = elements.sneaker__size;
+    const benefits = sneakerBenefitsInput;
+    const titleValue = title.value;
+    const priceValue = price.value;
+    const makersValue = makers.value;
+    const sizesValue = sizes.value;
+    const benefitsValue = benefits.value;
+    console.log(titleValue);
+    console.log(priceValue);
+    console.log(makersValue);
+    console.log(sizesValue);
+    console.log(benefitsValue);
+
+    if (titleValue && priceValue && makersValue && sizesValue && benefitsValue) {
+        // const sneakerArray = {
+        //     id: Math.floor(Math.random() * 1000),
+        //     title: titleValue,
+        //     img: "https://picsum.photos/300/200",
+        //     price: priceValue,
+        //     discountPrice: price * 0.75,
+        //     model: makersValue,
+        //     size: sizesValue,
+        //     addedDate: new Date(),
+        //     benefits: benefitsOptions
+        // }
+
+        const sneakerArray = {
+            id: Math.floor(Math.random() * 1000),
+            title: titleValue,
+            img: "https://picsum.photos/300/200",
+            price: Math.round(priceValue),
+            discountPrice: priceValue - (priceValue / 100 * 25),
+            model: makersValue,
+            size: sizesValue,
+            addedDate: new Date(),
+            benefits: benefitsOptions
+        }
+
+        addSneakerForm.reset();
+
+        const sneakerItem = renderSneakers(sneakerArray);
+        appendChild(sneakerList, sneakerItem)
+        showSneakers.push(sneakerArray);
+        sneakers.push(sneakerArray);
+        benefitsOptions = []
+    }
+
+    sneakerRefresh();
+    addSneakerModal.hide();
+
+});
